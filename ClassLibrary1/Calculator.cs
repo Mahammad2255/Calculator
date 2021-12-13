6ab8eabc9cb2822f246c9543112f35effde9afb9
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-13 14:03:36 -0800

    Add project files.

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        
+        public int Ustegel(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Cix(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Bol(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public int Vur(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+
+    }
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cl = new Calculator();
+            Console.WriteLine(cl.Ustegel(12, 6));
+            Console.WriteLine(cl.Cix(12, 6));
+            Console.WriteLine(cl.Vur(12, 6));
+            Console.WriteLine(cl.Bol(12, 6));
+        }
+    }
+}
